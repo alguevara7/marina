28e7ada2ff126f647181df632e4aecc99f3dc580
Guevara, Alexei <alguevara@ebay.com>
2015-07-02 20:18:21 -0400

    first commit

--- Marina/AppDelegate.swift
+++ Marina/AppDelegate.swift
@@ -7,6 +7,37 @@
 //
 
 import Cocoa
+import JavaScriptCore
+import AppKit
+import ApplicationServices
+import CoreFoundation
+
+
+/*
+
+registerWindowListener()
+
+WindowListener {
+    opened
+    closed
+    minimized
+    restored (from minimized or maximized)
+    maximized
+    focused
+}
+
+setWindowSize
+setWindowPosition
+
+... move to other desktop
+
+... implement focus follows mouse
+
+... when an app exists, close events for all its windows will be fired
+
+... each window has a layout
+
+*/
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
@@ -15,6 +46,36 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
+        let promptFlag = kAXTrustedCheckOptionPrompt.takeRetainedValue() as NSString
+        let dict: CFDictionary = [promptFlag: true]
+        AXIsProcessTrustedWithOptions(dict)
+        
+        
+        NSWorkspace.sharedWorkspace().notificationCenter.addObserverForName(
+            NSWorkspaceDidLaunchApplicationNotification, object: nil, queue: nil) { (aNotification:NSNotification!) -> Void in
+                
+                
+                let runningApp: NSRunningApplication?  = aNotification.userInfo![NSWorkspaceApplicationKey] as? NSRunningApplication
+                let pid: pid_t = runningApp!.processIdentifier
+//                NSLog("app = %u", pid)
+                //NSRunningApplication
+                
+                let app: Unmanaged<AXUIElementRef>! = AXUIElementCreateApplication(pid)
+                print(app.takeRetainedValue())
+                
+                //AXIsTrustedProcess
+                
+                
+                return
+        }
+        
+        let context = JSContext()
+        context.evaluateScript("var num = 5 + 5")
+        context.evaluateScript("var names = ['Grace', 'Ada', 'Margaret']")
+        context.evaluateScript("var triple = function(value) { return value * 3 }")
+        let tripleNum: JSValue = context.evaluateScript("triple(num)")
+        
+        println(tripleNum)
         // Insert code here to initialize your application
     }
 
